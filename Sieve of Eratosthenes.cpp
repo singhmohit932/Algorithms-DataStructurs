23f3919f3a22e319efb9bdfe9fc201ce355e8b6f
singhmohit932 <63581373+singhmohit932@users.noreply.github.com>
2021-07-24 20:31:53 +0530

    Add files via upload

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,41 @@
+//Sieving of Eratosthenes
+//O(nloglogn)
+int n;
+vector<bool> is_prime(n+1, true);
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i <= n; i++) {
+    if (is_prime[i] && (long long)i * i <= n) {
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+}
+
+
+//Different optimizations of the Sieve of Eratosthenes
+//nlnlnn−−√+o(n)
+
+int n;
+vector<bool> is_prime(n+1, true);
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i * i <= n; i++) {
+    if (is_prime[i]) {
+        for (int j = i * i; j <= n; j += i)
+            is_prime[j] = false;
+    }
+}
+
+// Sieve of Eratosthenes Having Linear Time Complexity
+
+const int N = 10000000;
+int lp[N+1];
+vector<int> pr;
+
+for (int i=2; i<=N; ++i) {
+    if (lp[i] == 0) {
+        lp[i] = i;
+        pr.push_back (i);
+    }
+    for (int j=0; j<(int)pr.size() && pr[j]<=lp[i] && i*pr[j]<=N; ++j)
+        lp[i * pr[j]] = pr[j];
+}
+
